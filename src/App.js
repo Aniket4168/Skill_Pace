eb7760ecbfeeacda8496c936633858aa12cb3b77
Aniket Agrawal <aniket.agra418@gmail.com>
2025-07-01 13:20:56 +0530

    New Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import Navbar from './components/common/Navbar';
 import Login from "./pages/Login"
 import SignUp from "./pages/SignUp"
+import ForgotPassword from './pages/ForgotPassword';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
           <Route path = "/" element = {<Home/>}/>
           <Route path = "/login" element = {<Login/>}/>
           <Route path = "/signup" element = {<SignUp/>}/>
+          <Route path = "/forgot-password" element = {<ForgotPassword/>}/>
       </Routes>
     </div>
   )
