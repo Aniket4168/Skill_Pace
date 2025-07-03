8b07415bbadbd7d09bac26800be3a53e49e87a9f
Aniket Agrawal <aniket.agra418@gmail.com>
2025-07-03 14:49:05 +0530

    add logout button

--- src/components/core/Auth/ProfileDropDown.jsx
+++ src/components/core/Auth/ProfileDropDown.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import {logout} from "../../../services/operations/authApi";
+import { useNavigate } from 'react-router-dom';
 
 const ProfileDropDown = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const logoutHandler =  () => {
+      dispatch(logout(navigate));
+  }
   return (
-    <div>ProfileDropDown</div>
+    <div>
+      <button onClick={logoutHandler}>
+        Logout
+      </button>
+    </div>
   )
 }
 
