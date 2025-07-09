b8a5bdb3de209a9a0651b6197e644a98b158b582
Aniket Agrawal <aniket.agra418@gmail.com>
2025-07-09 23:04:22 +0530

    Started contact us form

--- src/components/core/ContactPage/ContactUsForm.jsx
+++ src/components/core/ContactPage/ContactUsForm.jsx
@@ -1,8 +1,130 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form';
 const ContactUsForm = () => {
+
+  const [loading, setLoading] = useState(false);
+
+  const {
+    register,
+    handleSubmit, 
+    reset,
+    formState: {errors, isSubmitSuccessful}
+  } = useForm();
+
+  const submitContactForm = async(data) => {
+
+  }
+
+  useEffect( () => {
+    if(isSubmitSuccessful) {
+      reset({
+        email:"",
+        firstName:"",
+        lastName:"",
+        message:"",
+        phoneNo:"",
+      })
+    }
+  }, [reset, isSubmitSuccessful]);
+
   return (
-    <div>ContactUsForm</div>
+    <form onSubmit={handleSubmit(submitContactForm)}>
+
+      <div className="flex flex-col gap-7">
+        <div className='flex flex-col gap-5 lg:flex-row'>
+          {/* First name */}
+        <div className="flex flex-col gap-2 lg:w-[48%]">
+          <label htmlFor="firstname" className="lable-style">
+            First Name
+          </label>
+          <input
+            type="text"
+            name="firstname"
+            id="firstname"
+            placeholder="Enter first name"
+            className="form-style"
+            {...register("firstname", { required: true })}
+          />
+          {errors.firstname && (
+            <span className="-mt-1 text-[12px] text-yellow-100">
+              Please enter your name.
+            </span>
+          )}
+        </div>
+
+        {/* Last Name */}
+        <div className="flex flex-col gap-2 lg:w-[48%]">
+          <label htmlFor="lastname" className="lable-style">
+            Last Name
+          </label>
+          <input
+            type="text"
+            name="lastname"
+            id="lastname"
+            placeholder="Enter last name"
+            className="form-style"
+            {...register("lastname")}
+          />
+        </div>
+        </div>
+
+        {/* email */}
+        <div className="flex flex-col gap-2">
+          <label htmlFor="email" className="lable-style">
+            Email Address
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Enter email address"
+            className="form-style"
+            {...register("email", { required: true })}
+          />
+          {errors.email && (
+            <span className="-mt-1 text-[12px] text-yellow-100">
+              Please enter your Email address.
+            </span>
+          )}
+        </div>
+
+
+        {/* message */}
+        <div className="flex flex-col gap-2">
+          <label htmlFor="message" className="lable-style">
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            cols="30"
+            rows="7"
+            placeholder="Enter your message here"
+            className="form-style"
+            {...register("message", { required: true })}
+          />
+          {errors.message && (
+            <span className="-mt-1 text-[12px] text-yellow-100">
+              Please enter your Message.
+            </span>
+          )}
+        </div>
+
+        {/* button */}
+        <button
+          disabled={loading}
+          type="submit"
+          className={`rounded-md bg-yellow-50 px-6 py-3 text-center text-[13px] font-bold text-black shadow-[2px_2px_0px_0px_rgba(255,255,255,0.18)] 
+          ${
+            !loading &&
+            "transition-all duration-200 hover:scale-95 hover:shadow-none"
+          }  disabled:bg-richblack-500 sm:text-[16px] `}
+          >
+          Send Message
+        </button>
+      </div>
+
+    </form>
   )
 }
 
