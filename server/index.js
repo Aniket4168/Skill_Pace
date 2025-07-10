c8bc0ad9f3e97c278044993c5fd0d1ebf5ce3ad2
Aniket Agrawal <aniket.agra418@gmail.com>
2025-07-10 23:03:53 +0530

    Api errors fixed

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
+const contactRoutes = require("./routes/ContactUs")
 
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
@@ -40,6 +41,7 @@ cloudinaryConnect();
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/reach", contactRoutes );
 // app.use("/api/v1/payment", paymentRoutes);
 
 //default route
