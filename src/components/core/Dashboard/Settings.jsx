65660adaedd7c22eeca57c085e288253a7a8bbcf
Aniket Agrawal <aniket.agra418@gmail.com>
2025-07-25 23:51:28 +0530

    bug correction, half done

--- src/components/core/Dashboard/Settings.jsx
+++ src/components/core/Dashboard/Settings.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState } from 'react'
 import { useSelector } from 'react-redux'
-import { updateProfile, changePassword, updateDisplayPicture ,deleteProfile } from '../../../services/operations/SettingsApi'
+import { updateAdditionalDetails, updatePassword, updatePfp,deleteAccount } from '../../../services/operations/profileApi'
 import {AiOutlineEyeInvisible} from 'react-icons/ai'
 import {AiOutlineEye} from 'react-icons/ai'
 import { useDispatch } from 'react-redux'
@@ -17,7 +17,7 @@ const Settings = () => {
 
 
   //update profile picture
-  const pfp=useSelector(state=>state.profile.user.image);
+  const pfp=useSelector(state=>state.profile.user?.image);
   const [profilePicture, setprofilePicture] = useState(pfp)
   const token= useSelector(state=>state.auth.token);
 
@@ -25,13 +25,16 @@ const Settings = () => {
   const handleUpload = (e) => {
     e.preventDefault();
     const file = e.target[0].files[0];
-    updateDisplayPicture(token,file);
+    updatePfp(token,file);
   }
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     setprofilePicture(URL.createObjectURL(file));
   }
+
+
+
 
 
   //update additional info
@@ -53,7 +56,7 @@ const Settings = () => {
 
   const handelAdditionalDetails = (e) => {
     e.preventDefault()
-    updateProfile(token,formData);
+    updateAdditionalDetails(token,formData);
   }
 
 
@@ -81,7 +84,7 @@ const Settings = () => {
     e.preventDefault()
     const {newPassword, confirmPassword } = password;
     if (newPassword === confirmPassword) {
-      changePassword(token,password);
+      updatePassword(token,password);
     } else {
       alert("Password does not match")
     }
@@ -96,7 +99,7 @@ const Settings = () => {
   const onDeleteAccount = () => {
     if(window.confirm
       ("Are you sure you want to delete your account?")){
-        deleteProfile(token,dispatch,navigate);
+        deleteAccount(token,dispatch,navigate);
       }
 
   }
@@ -107,24 +110,23 @@ const Settings = () => {
     <div>
       <div className=' flex-1 overflow-auto'>
         <div className='mx-auto w-11/12 max-w-[1000px] py-10'>
-          <h1 className="mb-14 text-3xl font-medium text-richblack-5">Edit Profile</h1>
+        <h1 className="mb-14 text-3xl font-medium text-richblack-5">Edit Profile</h1>
 
-          {/* update profile picture */}
-          <div className='flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 md:p-8 md:px-12 px-3 py-3 text-richblack-5'>
-            <div className='flex items-center gap-x-4'>
-              <img className='aspect-square w-[78px] rounded-full object-cover'  src={profilePicture} alt='Profile_Image'></img>
-              <div className='space-y-2'>
-              <p>Change Profile Picture</p>
-
-              <form onSubmit={handleUpload}>
-                <div className='flex flex-row gap-3'>
-                  <label className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50'" htmlFor="upload">Select
-                <input id='upload' type="file" onChange={handleFileChange} className="hidden" accept="image/png, image/jpg, image/jpeg"/></label>
-                <button type='submit' className='flex items-center bg-yellow-50 cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 undefined'>Upload</button>
-                </div>
-              </form>
-              </div>
+        {/* update profile picture */}
+        <div className='flex items-center justify-between rounded-md border-[1px] border-richblack-700 bg-richblack-800 md:p-8 md:px-12 px-3 py-3 text-richblack-5'>
+          <div className='flex items-center gap-x-4'>
+            <img className='aspect-square w-[78px] rounded-full object-cover'  src={profilePicture} alt='aaa'></img>
+            <div className='space-y-2'>
+            <p>Change Profile Picture</p>
+            <form onSubmit={handleUpload}>
+            <div className='flex flex-row gap-3'>
+              <label className="cursor-pointer rounded-md bg-richblack-700 py-2 px-5 font-semibold text-richblack-50'" htmlFor="upload">Select
+            <input id='upload' type="file" onChange={handleFileChange} className="hidden" accept="image/png, image/gif, image/jpeg"/></label>
+             <button type='submit' className='flex items-center bg-yellow-50 cursor-pointer gap-x-2 rounded-md py-2 px-5 font-semibold text-richblack-900 undefined'>Upload</button>
             </div>
+            </form>
+            </div>
+          </div>
         </div>
         
 
